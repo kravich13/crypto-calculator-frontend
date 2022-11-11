d6bcbab02b7736961e2473d7773bf14699bffae9
Vladislav Onatskyi <vladislav.onatskiy@freshcodeit.net>
2022-11-11 23:15:28 +0200

    problems with redux were fixed, layout was fixed and stepper from MUI was implemented

--- src/components/signUp/SignUp.tsx
+++ src/components/signUp/SignUp.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Grid } from '@mui/material';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { SubmitHandler, useForm, useFormState } from 'react-hook-form';
 import { ILoginRequest } from '../../models/Auth';
 import { emailValidation, passwordValidation } from '../../validation/validation';
@@ -7,23 +7,45 @@ import { PasswordController } from '../shared';
 import { TextController } from '../shared/TextController';
 
 interface ISignUpProps {
-  onConfirm: SubmitHandler<ISignUpForm>;
+  onConfirm: SubmitHandler<ISignUpForm & INeedActivateAccount>;
 }
 
 export interface ISignUpForm extends ILoginRequest {
   confirmPassword: string;
 }
 
-export const SignUp: React.FC<ISignUpProps> = ({ onConfirm }) => {
+export interface INeedActivateAccount {
+  activateAccount: boolean;
+}
+
+export const SignUp: React.FC<ISignUpProps> = React.memo(({ onConfirm }) => {
   const { handleSubmit, control, resetField, getValues } = useForm<ISignUpForm>({ mode: 'onBlur' });
   const { errors, isValid } = useFormState({ control });
+
+  const [activateAccount, setActivateAccount] = useState(false);
 
   const onClear = useCallback(() => {
     resetField('email');
   }, [resetField]);
 
+  const onClickNext = useCallback(() => {
+    setActivateAccount(true);
+  }, []);
+
+  const onSubmit = useCallback(
+    (event: React.ChangeEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      handleSubmit((data) => {
+        onConfirm({ ...data, activateAccount });
+      })();
+      setActivateAccount(false);
+    },
+    [activateAccount]
+  );
+
   return (
-    <Box component="form" noValidate onSubmit={handleSubmit(onConfirm)} sx={{ mt: 3 }}>
+    <Box component="form" noValidate onSubmit={onSubmit} sx={{ mt: 3 }}>
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <TextController
@@ -61,15 +83,26 @@ export const SignUp: React.FC<ISignUpProps> = ({ onConfirm }) => {
         </Grid>
       </Grid>
 
-      <Button
-        sx={{ mt: 3, mb: 2, textTransform: 'none' }}
-        type="submit"
-        fullWidth
-        variant="contained"
-        disabled={!isValid}
-      >
-        Sign up
-      </Button>
+      <Box style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Button
+          sx={{ mt: 3, mb: 2, textTransform: 'none', width: '120px' }}
+          type="submit"
+          variant="contained"
+          disabled={!isValid}
+        >
+          Sign up now
+        </Button>
+
+        <Button
+          sx={{ mt: 3, mb: 2, textTransform: 'none', width: '120px' }}
+          type="submit"
+          variant="contained"
+          disabled={!isValid}
+          onClick={onClickNext}
+        >
+          Next
+        </Button>
+      </Box>
     </Box>
   );
-};
+});
