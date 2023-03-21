95580e65065b839384d39c704a0eae4142332f54
Vladislav Onatskyi <vladislav.onatskiy@freshcodeit.net>
2023-03-21 19:46:07 +0200

    price chart component has been implemented

--- src/features/components/index.ts
+++ src/features/components/index.ts
@@ -2,5 +2,6 @@ export * from './DetailedInvestmentStatistics';
 export * from './EmailCode';
 export * from './GeneralInvestmentStatistics';
 export * from './PeriodAndAmount';
+export * from './PriceChart';
 export * from './SelectedInvestCoins';
 export * from './SendEmail';
