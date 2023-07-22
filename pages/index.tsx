c82f6ac9fac7de28b07d18087752ed860e14f649
Vladislav Onatskyi <vladislav.onatskiy@freshcodeit.net>
2023-07-22 10:58:47 +0300

    test commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
 
+sds;
+
 export default function Home() {
   const { t } = useTranslation();
 
